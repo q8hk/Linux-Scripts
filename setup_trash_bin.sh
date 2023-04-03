20e9452fb127f1eba259a2d80edaaa47230c6d86
Abdullah <delpieroq7@gmail.com>
2023-04-03 22:19:09 +0300

    changed bash to zsh

--- setup_trash_bin.sh
+++ setup_trash_bin.sh
@@ -1,20 +1,23 @@
-#!/bin/bash
+#!/bin/zsh
 
 # Create the trash bin directory
 mkdir ~/.trash
 
 # Add an alias for the rm command to move files to the trash bin
-echo 'alias rm="mv --target-directory=$HOME/.trash"' >> ~/.bashrc
+echo 'alias rm="mv --target-directory=$HOME/.trash"' >> ~/.zshrc
 
 # Reload the shell configuration
-source ~/.bashrc
+source ~/.zshrc
 
 # Create a script to empty the trash bin
-echo '#!/bin/bash
-rm -rf $HOME/.trash/*' > ~/empty_trash.sh
+echo '#!/bin/zsh
+rm -rf $HOME/.trash/*' > empty_trash.sh
+
+# Move the script to /usr/local/bin
+sudo mv empty_trash.sh /usr/local/bin/
 
 # Make the script executable
-chmod +x ~/empty_trash.sh
+sudo chmod +x /usr/local/bin/empty_trash.sh
 
 # Set up a cron job to run the script once a month
-(crontab -l 2>/dev/null; echo "0 0 1 * * $HOME/empty_trash.sh") | crontab -
+(crontab -l 2>/dev/null; echo "0 0 1 * * /usr/local/bin/empty_trash.sh") | crontab -
